173b9a547ada11bec4382732b5a1d1a001559892
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-07 21:41:07 -0500

    not sure why put isnt passing

--- api/server.js
+++ api/server.js
@@ -39,10 +39,41 @@ User.insert(body)
 .catch(() =>{
     res.status(500).json({message:"There was an error while saving the user to the database"})
 })
-
 })
 
+//PUT
+server.put(` /api/users/:id `, (req , res) =>{
+const user = req.body;
+if(!req.body.name || !req.body.bio) {
+    res.status(400).json({ message:  "Please provide name and bio for the user" });
+    return;
+}
+User.update(req.params.id, user)
+.then(users =>{
+    if(users == null){
+        res.status(404).json({message:"The user with the specified ID does not exist"})
+        return;
+    }else{res.status(200).json(user)}
+})
+.catch(err =>{
+    res.status(500).json({message:"The user information could not be modified"})
+})
+})
+server.delete('/api/users/:id', (req, res) => {
+    
+    User.remove(req.params.id)
+        .then(user => {
+            if(user == null) {
+                res.status(404).json({ message: `does not exist` });
+                
+            } else {res.status(200).json(user);}
 
+            
+        })
+        .catch(() => {
+            res.status(400).json({ message: `could not delete user!` });
+        });
+});
 
 
 
