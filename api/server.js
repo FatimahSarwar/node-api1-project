7629c905e111f8817c8be8004bd9a69fb6e71ae8
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-07 20:43:06 -0500

    finished with post

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ User.find()
     res.json(users);
 });
 });
-
+//GET
 server.get('/api/users/:id', (req, res) => {
    User.findById(req.params.id)
     .then(user => {
@@ -23,7 +23,24 @@ server.get('/api/users/:id', (req, res) => {
         res.status(400).json({message: `Could not get user`})
     })
 })
+//POST
+server.post('/api/users', (req, res) =>{
+    const body = req.body;
+    if(!req.body.name || !req.body.bio) {
+        res.status(400).json({ message:  "Please provide name and bio for the user" });
+        return;
+    }
+  
 
+User.insert(body)
+.then(user =>{
+    res.status(201).json(user);
+})
+.catch(() =>{
+    res.status(500).json({message:"There was an error while saving the user to the database"})
+})
+
+})
 
 
 
