ba489958f739552fd8fb90f25c408f82c3eb26b2
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-07 20:27:07 -0500

    finished with get endpoints

--- api/server.js
+++ api/server.js
@@ -1,3 +1,32 @@
+const express = require('express')
+const server = express();
+const User = require('./users/model')
 // BUILD YOUR SERVER HERE
+server.use(express.json());
+server.get('/api/users',(req, res) => {
+User.find()
+.then(users => {
+    res.json(users);
+});
+});
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users/:id', (req, res) => {
+   User.findById(req.params.id)
+    .then(user => {
+        if(user == null){
+            res.status(404).json({message: `does not exist`})
+        }else{
+            res.json(user)
+        }
+    })
+    .catch(() => {
+        res.status(400).json({message: `Could not get user`})
+    })
+})
+
+
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
